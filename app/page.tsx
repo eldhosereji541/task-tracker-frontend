0ffa7c09ed0ff9de0b954ec8d1cde61579d63aa2
Eldhose Reji <106137724+eldhosereji541@users.noreply.github.com>
2026-03-22 15:19:35 +0900

    user registration and login: added login and registration form and related functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,80 @@
-import Image from "next/image";
+"use client";
+
+import { LOGIN, REGISTER } from "@/src/lib/graphql";
+import { useMutation } from "@apollo/client/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [isLogin, setIsLogin] = useState(true);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const [login] = useMutation<{ login: { token: string } }>(LOGIN);
+  const [register] = useMutation<{ register: { token: string } }>(REGISTER);
+
+  const handleSubmit = async () => {
+    try {
+      const dfn = isLogin ? login : register;
+      const input = isLogin
+        ? {
+          email: form.email,
+          password: form.password,
+        }
+        : form;
+
+      const { data } = await dfn({
+        variables: { input },
+      });
+      if (!data) return;
+      const token = isLogin ? (data as { login: { token: string } }).login.token : (data as { register: { token: string } }).register.token;
+      localStorage.setItem("token", token);
+      !isLogin ? window.location.reload() : window.location.href = "/tasks";
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="min-h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md text-gray-700">
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">{isLogin ? "Login" : "Register"}</h1>
+        {!isLogin && (
+          <input
+            type="text"
+            placeholder="Name"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            className="w-full p-3 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          className="w-full p-3 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <input
+          type="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          className="w-full p-3 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          onClick={handleSubmit}
+          className="w-full bg-teal-800 text-white p-3 rounded-md hover:bg-teal-600 transition duration-200 mb-4">
+          {isLogin ? "Login" : "Register"}
+        </button>
+        <button
+          onClick={() => setIsLogin(!isLogin)}
+          className="w-full text-teal-500 hover:text-teal-700 transition duration-200">
+          Switch to {isLogin ? "Register" : "Login"}
+        </button>
+      </div>
     </div>
   );
 }
---
+++ app/tasks/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function TasksPage() {
+
+  return (
+    <div className="p-10">
+      tasks
+    </div>
+  );
+}
